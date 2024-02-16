d724d2c96c39dfa49210e4ff3c458a848e19230c
HiperDev42 <fekelemen@gmail.com>
2024-02-16 00:05:20 -0300

    feat: add wallet & tx signature

--- pycoin/tx.py
+++ pycoin/tx.py
@@ -1,6 +1,8 @@
 import pycoin.utils as utils
 import json
 from hashlib import sha256
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
 
 
 class Tx:
@@ -14,6 +16,11 @@ class Tx:
         self.receiver = receiver
         self.amount = amount
         self.timestamp = timestamp
+
+    def sign(self, key: RSA.RsaKey):
+        sig_scheme = pkcs1_15.new(key)
+        self.signature = sig_scheme.sign(self.hash)
+        return self.signature
 
     def toJSON(self):
         return json.dumps(self, cls=utils.Encoder, sort_keys=True)
