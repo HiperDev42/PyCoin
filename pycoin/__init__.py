6699fe4506dbdb2e437077ce06f0313acaa1e00e
Felipe Kelemen <fekelemen@gmail.com>
2024-03-28 21:55:25 -0300

    fix: remove old node

--- pycoin/__init__.py
+++ pycoin/__init__.py
@@ -1,5 +1,4 @@
 from pycoin.blockchain import Blockchain, Block
-from pycoin.protocol import Connection, Message
 from pycoin.tx import Tx
 from pycoin.logs import logger
 
@@ -8,6 +7,5 @@ class NotImplementedError(Exception):
     ...
 
 
-__all__ = ["Blockchain", "Block", "Connection",
-           "Message", "NotImplementedError", "logger", "Tx"]
+__all__ = ["Blockchain", "Block", "NotImplementedError", "logger", "Tx"]
 __version__ = "0.1.0"
